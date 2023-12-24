7e4e6c8d93e4e386b13b574c2e0f63752280e596
rikaisokai <81399842+rikaisokai@users.noreply.github.com>
2023-12-24 15:46:48 -0800

    Add files via upload
    This code is similiar to having data sent to a dump file after inputs on a console in my opinion. What is actually identifies as is a program taking a list of integers and chars for example:
    
    618 phi
    777 boeing
    737 boeingoriginal
    
    and seperating the integers into arrays and then placing the chars next to it also in the same array. Afterwards it places this list into an output file. Sorta gives you an understanding of how consoles and dump files work

---
+++ linkedlistoutput.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <list>
+
+struct Node {
+    char character;
+    Node* next;
+    Node(char ch) : character(ch), next(nullptr) {}
+};
+
+int main() {
+
+    // Open the input file
+    std::ifstream inputFile("input.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening the input file." << std::endl;
+        return 1;
+    }
+
+    // Open the output file for the linked list
+    std::ofstream outputListFile("output_list.txt");
+
+    if (!outputListFile.is_open()) {
+        std::cerr << "Error opening the output file for the linked list." << std::endl;
+        inputFile.close();  // Close the input file
+        return 1;
+    }
+
+    // Initialize variables
+    char ch;
+    int num;
+    std::unordered_map<int, int> countMap;
+    std::list<Node*> linkedList;
+
+    // Read characters from the input file
+    while (inputFile.get(ch)) {
+        if (isdigit(ch)) {
+            // Convert the character to an integer
+            num = ch - '0';
+
+            // Display the array number
+            std::cout << "Array[" << num << "] ";
+
+            // Count the occurrences
+            countMap[num]++;
+        } else if (isalpha(ch)) {
+            // Display the character
+            std::cout << ch << " ";
+
+            // Add character to the linked list
+            linkedList.push_back(new Node(ch));
+        } else if (ch == '\n') {
+            // Move to a new line
+            std::cout << "\n";
+        }
+    }
+
+    // Close the input file
+    inputFile.close();
+
+    // Display the count of each integer
+    std::cout << "\nInteger Counts:\n";
+    for (const auto& entry : countMap) {
+        std::cout << "Array[" << entry.first << "]: " << entry.second << "\n";
+    }
+
+    // Display the characters in the linked list and write to the output file
+    std::cout << "\nLinked List Characters:\n";
+    for (Node* node : linkedList) {
+        std::cout << node->character << " ";
+        outputListFile << node->character << " ";
+        delete node;  // Clean up allocated nodes
+    }
+
+    // Close the output file for the linked list
+    outputListFile.close();
+
+    return 0;
+}
